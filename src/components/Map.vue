9c3ae417fe78f4ec7e393c6ff9214db6f0e07fc8
Joey Burgers <joey.burgers@snow.nl>
2019-03-27 14:52:39 +0100

    eerste vue project opgezet met mapbox erin verwerkt

---
+++ src/components/Map.vue
@@ -0,0 +1,38 @@
+
+<template>
+    <div id="map">
+        <mapbox access-token="pk.eyJ1Ijoiam9leWJ1cmdlcnMwOTkiLCJhIjoiY2p0cG9iOTZtMDY2azRlczdybHU5OTU2eiJ9.BScZE0DWOamSxZpiYzf4pQ"
+            :map-options="{
+                container: 'map', // container id
+                style: 'mapbox://styles/mapbox/streets-v11',  // stylesheet location
+                center: [7.426644, 43.740070], // starting position [lng, lat]
+                zoom: 13 // starting zoom
+            }">
+        </mapbox>
+    </div>
+</template>
+
+<script>
+    import Mapbox from 'mapbox-gl-vue';
+    export default {
+        name: 'app',
+        components: {
+            'mapbox': Mapbox
+        },
+        data () {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        }
+    }
+</script>
+
+<style>
+    body { margin:0; padding:0; }
+    #map { position:absolute; height: 100%; width:100%; }
+
+    #app {
+        width: 100%;
+        height: 500px;
+    }
+</style>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Mapbox from 'mapbox-gl-vue'
+
+Vue.config.productionTip = false
+
+
+/* eslint-disable no-new */
+const app = new Vue({
+  el: '#app',
+  router,
+  components: {
+    App,
+    Mapbox
+  },
+  template: '<App/>'
+})
